733bad0de03005b48ce5485fe7ee94e0fe925152
Corinne Hoffmann <corinne.hoffmann@axa.fr>
2020-04-03 14:32:51 +0200

    derniers tests sur exception

--- src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerIT.java
+++ src/test/java/com/safetynet/alerts/controller/MedicalRecordControllerIT.java
@@ -39,55 +39,48 @@ import com.safetynet.alerts.services.MedicalRecordService;
 class MedicalRecordControllerIT {
 
 	MedicalRecord medicalRecord = new MedicalRecord();
-	
+
 	@Autowired
 	private MockMvc mockmvc;
-	
+
 	@Autowired
 	private MedicalRecordService medicalRecordService;
-	
+
 	@Autowired
 	private MedicalRecordDao medicalRecordDao;
-	
+
 	@BeforeEach
 	void SetUpEnv() {
 		medicalRecordDao.deleteAllMedicalRecord();
 	}
-	
-	/*
+
 	@Test
 	void shouldGetMedicalRecordWhenEmpty() throws Exception {
 
-		MvcResult result = mockmvc.perform(get("/medicalrecord"))
-								.andExpect(status().isOk())
-								.andDo(print())
-								.andReturn();	
+		MvcResult result = mockmvc.perform(get("/medicalrecord")).andExpect(status().isOk()).andDo(print()).andReturn();
 	}
-	
+
 	@Test
 	void shouldGetMedicalRecord() throws Exception {
-		
+
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		MedicalRecord medicalRecord1 = medicalRecordService.createMedicalRecord(medicalRecord);
-		
-		MvcResult result = mockmvc.perform(get("/medicalrecord"))
-								.andExpect(status().isOk())
-								.andDo(print())
-								.andReturn();
+
+		MvcResult result = mockmvc.perform(get("/medicalrecord")).andExpect(status().isOk()).andDo(print()).andReturn();
 	}
 
 	@Test
@@ -100,234 +93,208 @@ class MedicalRecordControllerIT {
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecordForReturnFormat.setFirstName("Totocreate");
 		medicalRecordForReturnFormat.setLastName("Totocreate name");
 		medicalRecordForReturnFormat.setBirthdate(stringDate);
 		medicalRecordForReturnFormat.setMedications(medications);
-		medicalRecordForReturnFormat.setAllergies(allergies);		
-		
+		medicalRecordForReturnFormat.setAllergies(allergies);
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String medicalRecordJson = objectMapper.writeValueAsString(medicalRecordForReturnFormat);
-	
-		this.mockmvc.perform(post("/medicalrecord")
-	            .contentType(MediaType.APPLICATION_JSON)
-	            .content(medicalRecordJson))
-	            .andExpect(status().isOk()).andDo(print()).andReturn();
+
+		this.mockmvc.perform(post("/medicalrecord").contentType(MediaType.APPLICATION_JSON).content(medicalRecordJson))
+				.andExpect(status().isOk()).andDo(print()).andReturn();
 	}
-	
+
 	@Test
 	void shouldUpdatePerson() throws Exception {
-		
+
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
+
 		String stringDateUpdated = "03/06/1990";
 		MedicalRecordForReturnFormat medicalRecordForReturnFormat = new MedicalRecordForReturnFormat();
 		medicalRecordForReturnFormat.setFirstName("Totocreate");
 		medicalRecordForReturnFormat.setLastName("Totocreate name");
 		medicalRecordForReturnFormat.setBirthdate(stringDateUpdated);
 		medicalRecordForReturnFormat.setMedications(medications);
-		medicalRecordForReturnFormat.setAllergies(allergies);	
-		
+		medicalRecordForReturnFormat.setAllergies(allergies);
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String medicalRecordJson = objectMapper.writeValueAsString(medicalRecordForReturnFormat);
-		
-		MvcResult result = mockmvc.perform(	put("/medicalrecord")
-										.contentType(MediaType.APPLICATION_JSON)
-										.content(medicalRecordJson)
-										.characterEncoding("utf-8"))
-								.andExpect(status().isOk())
-								.andDo(print())
-								.andReturn();
-	} 
+
+		MvcResult result = mockmvc.perform(put("/medicalrecord").contentType(MediaType.APPLICATION_JSON)
+				.content(medicalRecordJson).characterEncoding("utf-8")).andExpect(status().isOk()).andDo(print())
+				.andReturn();
+	}
+
 	@Test
 	void shouldNotUpdatePersonBecauseNotFound() throws Exception {
-		
+
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
+
 		String stringDateUpdated = "03/06/1990";
 		MedicalRecordForReturnFormat medicalRecordForReturnFormat = new MedicalRecordForReturnFormat();
 		medicalRecordForReturnFormat.setFirstName("Toto1");
 		medicalRecordForReturnFormat.setLastName("Toto1 name");
 		medicalRecordForReturnFormat.setBirthdate(stringDateUpdated);
 		medicalRecordForReturnFormat.setMedications(medications);
-		medicalRecordForReturnFormat.setAllergies(allergies);	
-		
+		medicalRecordForReturnFormat.setAllergies(allergies);
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String medicalRecordJson = objectMapper.writeValueAsString(medicalRecordForReturnFormat);
-		
-		MvcResult result = mockmvc.perform(	put("/medicalrecord")
-										.contentType(MediaType.APPLICATION_JSON)
-										.content(medicalRecordJson)
-										.characterEncoding("utf-8"))
-								.andExpect(status().isNotFound())
-								.andDo(print())
-								.andReturn();
-	} 
-	
+
+		MvcResult result = mockmvc.perform(put("/medicalrecord").contentType(MediaType.APPLICATION_JSON)
+				.content(medicalRecordJson).characterEncoding("utf-8")).andExpect(status().isNotFound()).andDo(print())
+				.andReturn();
+	}
+
 	@Test
 	void shouldDeleteMedicalRecord() throws Exception {
 
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
-		MvcResult result = mockmvc.perform(delete("/medicalrecord")
-										.param("firstName", "Totocreate")
-										.param("lastName", "Totocreate name")
-										.characterEncoding("utf-8"))
-									.andExpect(status().isOk())
-									.andDo(print())
-									.andReturn();
-	}	
+
+		MvcResult result = mockmvc.perform(delete("/medicalrecord").param("firstName", "Totocreate")
+				.param("lastName", "Totocreate name").characterEncoding("utf-8")).andExpect(status().isOk())
+				.andDo(print()).andReturn();
+	}
+
 	@Test
 	void shouldNotDeleteMedicalRecordBecauseNotFound() throws Exception {
 
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
-		MvcResult result = mockmvc.perform(delete("/medicalrecord")
-										.param("firstName", "Toto1")
-										.param("lastName", "Toto1 name")
-										.characterEncoding("utf-8"))
-									.andExpect(status().isNotFound())
-									.andDo(print())
-									.andReturn();
-	}	
-	
+
+		MvcResult result = mockmvc.perform(delete("/medicalrecord").param("firstName", "Toto1")
+				.param("lastName", "Toto1 name").characterEncoding("utf-8")).andExpect(status().isNotFound())
+				.andDo(print()).andReturn();
+	}
+
 	@Test
 	void shouldNotDeleteMedicalRecordBecauseNotParam() throws Exception {
 
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
-		MvcResult result = mockmvc.perform(delete("/medicalrecord")
-										.param("firstName", "Totocreate")
-										.characterEncoding("utf-8"))
-									.andExpect(status().isInternalServerError())
-									.andDo(print())
-									.andReturn();
-	}	
-	
+
+		MvcResult result = mockmvc
+				.perform(delete("/medicalrecord").param("firstName", "Totocreate").characterEncoding("utf-8"))
+				.andExpect(status().isInternalServerError()).andDo(print()).andReturn();
+	}
+
 	@Test
 	void shouldNotFindCompletePath() throws Exception {
-	
-	MvcResult result = mockmvc.perform(get("/medicalRecord"))
-			.andExpect(status().isNotFound())
-			.andDo(print())
-			.andReturn();	
-}*/
-	
+
+		MvcResult result = mockmvc.perform(get("/medicalRecord")).andExpect(status().isNotFound()).andDo(print())
+				.andReturn();
+	}
+
 	@Test
 	void shouldNotCreateExistingMedicalRecord() throws Exception {
 
 		String stringDate = "03/05/2000";
-		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");		
+		DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 		Date birthDay = dateFormat.parse(stringDate);
 		List<String> medications = new ArrayList<String>();
 		medications.add("hydrapermazol:100mg");
 		List<String> allergies = new ArrayList<String>();
 		allergies.add("nillacilan");
 		allergies.add("peanut");
-		
+
 		medicalRecord.setFirstName("Totocreate");
 		medicalRecord.setLastName("Totocreate name");
 		medicalRecord.setBirthdate(birthDay);
 		medicalRecord.setMedications(medications);
-		medicalRecord.setAllergies(allergies);	
-		
+		medicalRecord.setAllergies(allergies);
+
 		medicalRecordService.createMedicalRecord(medicalRecord);
-		
-		//Creation identique 
+
+		// Creation identique
 		MedicalRecordForReturnFormat medicalRecordForReturnFormat = new MedicalRecordForReturnFormat();
-		
+
 		medicalRecordForReturnFormat.setFirstName("Totocreate");
 		medicalRecordForReturnFormat.setLastName("Totocreate name");
 		medicalRecordForReturnFormat.setBirthdate(stringDate);
 		medicalRecordForReturnFormat.setMedications(medications);
-		medicalRecordForReturnFormat.setAllergies(allergies);		
-		
+		medicalRecordForReturnFormat.setAllergies(allergies);
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		String medicalRecordJson = objectMapper.writeValueAsString(medicalRecordForReturnFormat);
-	
-		this.mockmvc.perform(post("/medicalrecord")
-	            .contentType(MediaType.APPLICATION_JSON)
-	            .content(medicalRecordJson))
-	            .andExpect(status().isFound())
-	            .andDo(print())
-	            .andReturn();
+
+		this.mockmvc.perform(post("/medicalrecord").contentType(MediaType.APPLICATION_JSON).content(medicalRecordJson))
+				.andExpect(status().isFound()).andDo(print()).andReturn();
 	}
-	
-	
+
 }
--- src/test/java/com/safetynet/alerts/exception/CustomResponseEntityExceptionHandlerTest.java
+++ src/test/java/com/safetynet/alerts/exception/CustomResponseEntityExceptionHandlerTest.java
@@ -1,16 +1,24 @@
 package com.safetynet.alerts.exception;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.text.ParseException;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.yaml.snakeyaml.parser.ParserException;
 
 import com.safetynet.alerts.AlertsApplication;
 import com.safetynet.alerts.exception.CustomResponseEntityExceptionHandler;
 import com.safetynet.alerts.exception.DaoException;
+import com.safetynet.alerts.model.ApiResponse;
 
 
 @SpringBootTest(classes = AlertsApplication.class)
@@ -54,6 +62,28 @@ class CustomResponseEntityExceptionHandlerTest {
 		assertEquals(HttpStatus.BAD_REQUEST,
 				customResponseEntityExceptionHandler.handleControllerException(ex).getHttpStatus());
 		assertEquals(message, customResponseEntityExceptionHandler.handleControllerException(ex).getMessage());
+
+	}
+	
+	@Test
+	public void whenHandleParseException() {
+		String message = "JSON_ERROR_CONVERSION_DATE";
+		int errorOffset=0;
+		ParseException ex = new ParseException(message,errorOffset);
+
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,
+				customResponseEntityExceptionHandler.handleParseException(ex).getHttpStatus());
+		assertEquals(message, customResponseEntityExceptionHandler.handleParseException(ex).getMessage());
+	}
+	
+	@Test
+	public void whenhandleFoundException() {
+		String message = "OBJET_FOUND";
+		DaoCreationException ex = new DaoCreationException(message);
+
+		assertEquals(HttpStatus.FOUND,
+				customResponseEntityExceptionHandler.handleFoundException(ex).getHttpStatus());
+		assertEquals(message, customResponseEntityExceptionHandler.handleFoundException(ex).getMessage());
 
 	}
 }
